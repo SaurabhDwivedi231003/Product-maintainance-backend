861c5e8026fb4c56809a70cf282e88133cb47fda
SAURABH DWIVEDI <saurabhdwivedi2310@gmail.com>
2024-05-13 01:11:56 +0530

    deploy

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    mongoURI: 'mongodb+srv://saurabhdwivedi2310:12344321@cluster0.hkavumb.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0',
+    JWT_SECRET: 'sccevcysxwvxreqcsd25##$%$2354*^ET#!@#$V5850saurbcyefewk'
+  };
+  
