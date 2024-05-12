4960f284c82f67b4c1f65f434b5a1ade9eeb66ae
SAURABH DWIVEDI <saurabhdwivedi2310@gmail.com>
2024-05-13 03:44:20 +0530

    deplop update

--- server.js
+++ server.js
@@ -1,22 +1,23 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const authMiddleware = require('./middleware/auth'); // Import your middleware here
 const productRoutes = require('./routes/products');
 const authRoutes = require('./routes/auth');
-const authMiddleware = require('./middleware/auth'); // Import your middleware here
 require('dotenv').config();
 
-const app = express();
+const app = express();  
    
 // Middleware  
 app.use(bodyParser.json());
 app.use(cors());  
-       
+
+// Authentication middleware
+app.use('/api/products', authMiddleware);
+
 // Routes  
 app.use('/api/auth', authRoutes);
-app.use('/api/products', authMiddleware, productRoutes); // Use your middleware here
-
-
+app.use('/api/products', productRoutes); // Use your middleware here
 
 //Start server
 const PORT = process.env.PORT || 5000;
